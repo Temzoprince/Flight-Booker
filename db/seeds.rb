5a5584e199a932d5341736008bc16381db4e8174
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-08-06 09:47:29 +0100

    Airport model with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+airports = {
+  0 => {
+    :name => 'London Heathrow',
+    :code => 'LHR',
+    :city => 'London',
+    :country => 'UK'
+  },
+  1 => {
+    :name => 'London Gatwick',
+    :code => 'LGW',
+    :city => 'London',
+    :country => 'UK'
+  },
+  2 => {
+    :name => 'Manchester',
+    :code => 'MAN',
+    :city => 'Manchester',
+    :country => 'UK'
+  },
+  3 => {
+    :name => 'Los Angeles International',
+    :code => 'LAX',
+    :city => 'Los Angeles',
+    :country => 'USA'
+  },
+  4 => {
+    :name => 'Denver International',
+    :code => 'DEN',
+    :city => 'Denver',
+    :country => 'USA'
+  },
+  5 => {
+    :name => 'John F Kennedy',
+    :code => 'JFK',
+    :city => 'New York',
+    :country => 'USA'
+  }
+}
+
+airports.each_value do |airport|
+  Airport.create_or_find_by(code: airport[:code],
+                            name: airport[:name],
+                            city: airport[:city],
+                            country: airport[:country])
+end
